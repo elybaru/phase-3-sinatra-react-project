a6f20e23d65cd562113d79941bb291b6e4665ad3
Ely Karr <elykarr@gmail.com>
2022-02-28 12:33:17 -0800

    creating some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,35 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+### Create poets
+rumi= Poet.create(name: "Rumi")
+
+### Create poems
+
+rumi.poems.create(title: "The Guest House", content: "
+This being human is a guest house.
+Every morning a new arrival.
+
+A joy, a depression, a meanness,
+some momentary awareness comes
+as an unexpected visitor.
+
+Welcome and entertain them all!
+Even if they’re a crowd of sorrows,
+who violently sweep your house
+empty of its furniture,
+still, treat each guest honorably.
+He may be clearing you out
+for some new delight.
+
+The dark thought, the shame, the malice,
+meet them at the door laughing,
+and invite them in.
+
+Be grateful for whoever comes,
+because each has been sent
+as a guide from beyond.
+
+")
+
 puts "✅ Done seeding!"
