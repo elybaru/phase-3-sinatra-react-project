bde9f2b45bfdb0ff7e22deaef5833f46ae413079
Ely Karr <elykarr@gmail.com>
2022-02-28 20:52:55 -0800

    building out front end form

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+    const defaultPoemForm = {
+        title: "",
+        author: "",
+        body: ""
+    }
+    const [createPoem, setCreatePoem] = useState(defaultPoemForm)
+
+    const handleFormChange = (e) => {
+        setCreatePoem({ ...createPoem, [e.target.name]: e.target.value })
+    }
+
+
     return (
         <div>
-            <h1>I am the create component, create a new poem here"</h1>
+            <h2>Create Poem</h2>
+            <p>Create or submit a new poem below</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Title
+                <input
+                        type='text'
+                        name='title'
+                        value={createPoem.title}
+                        onChange={handleFormChange}
+                    />
+                </label>
+            </form>
         </div>
     )
 }
