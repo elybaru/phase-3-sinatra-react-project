29525b8e86d223049155a31cd5d240f2ef98fbcd
Ely Karr <elykarr@gmail.com>
2022-02-28 16:04:38 -0800

    navbar routes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,16 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
+            <div>
+                <h1 className="logo">Verses</h1>
+            </div>
+            <div>
+                <NavLink to="/" className="menuItems">Home</NavLink>
+                <NavLink to="/poems" className="menuItems">Poems</NavLink>
+                <NavLink to="/poets" className="menuItems">Poets</NavLink>
+                <NavLink to="/create" className="menuItems">Create</NavLink>
+                <NavLink to="/favorites" className="menuItems">Favorites</NavLink>
+            </div>
 
         </div>
     )
